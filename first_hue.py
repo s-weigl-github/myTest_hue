02d3e6c8295fc9983b5a4dd1f585ba108b87c3df
s.weigl.git <s.weigl@linux-himmel.org>
2016-10-05 20:00:49 +0200

    added infos

--- first_hue.py
+++ first_hue.py
@@ -1,3 +1,9 @@
+##########################
+### just the first attempt
+### there will be more in no time
+### i hope :-)
+### covering the basics for now
+##########################
 
 from phue import Bridge
 from time import sleep
@@ -13,3 +19,6 @@ sleep(5)
 b.set_light(['flur', 'küche'], 'bri', 12)
 b.set_light(['flur', 'zimmer', 'küche'], 'sat', 254)
 b.set_light(['flur', 'zimmer', 'küche'], 'hue', 15000)
+
+
+# E-o-F
