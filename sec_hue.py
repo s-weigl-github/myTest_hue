c8b12082e9f7b00337ae4f0b7289bc1d433981cf
s.weigl.git <s.weigl@linux-himmel.org>
2016-10-09 18:28:44 +0200

    different color set

---
+++ sec_hue.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+##########################
+### just the first attempt
+### there will be more in no time
+### i hope :-)
+### covering the basics for now
+##########################
+
+from phue import Bridge
+from time import sleep
+
+b = Bridge(ip='192.168.61.21')
+
+b.connect()
+
+b.get_api()
+
+b.set_light(['zimmer'], 'bri', 80)
+sleep(5)
+b.set_light(['flur', 'küche'], 'bri', 80)
+b.set_light(['flur', 'zimmer', 'küche'], 'sat', 45)
+b.set_light(['flur', 'zimmer', 'küche'], 'hue', 235)
+
+
+# E-o-F
