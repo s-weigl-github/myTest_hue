4ecbbdc3165bebe04334e06a065c535673083355
s.weigl.git <s.weigl@linux-himmel.org>
2017-04-20 17:36:39 +0200

    little change

--- first_hue.py
+++ first_hue.py
@@ -15,7 +15,7 @@ b.connect()
 b.get_api()
 
 b.set_light(['zimmer'], 'bri', 75) #75
-sleep(5)
+sleep(2)
 b.set_light(['flur', 'küche'], 'bri', 38) #38
 b.set_light(['flur', 'zimmer', 'küche'], 'sat', 254)
 b.set_light(['flur', 'zimmer', 'küche'], 'hue', 15000)
